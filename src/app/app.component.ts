3d150aee371381da91b671c837cf6f83d9db55da
Raja Taimur <rajataimur794@gmail.com>
2023-08-18 13:01:39 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RegistrationFormApp';
+  user :{
+    firstName: string,
+    lastName: string,
+    email: string,
+    mobile: string,
+    address: string
+  }  = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobile: '',
+    address: ''
+  };
+
+  registeredUsers: any[] = [];
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.registeredUsers.push({ ...this.user });
+    this.user = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobile: '',
+      address: ''
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
